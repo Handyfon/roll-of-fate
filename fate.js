e85b786f49208ad10cec8ca107d4628ea49e6b39
Handyfon <Handyfon@hotmail.com>
2020-05-16 22:57:11 +0200

    0.2 for Foundry 0.5.7

--- fate.js
+++ fate.js
@@ -1,7 +1,7 @@
 Hooks.once('init', function() {
 
     game.settings.register('fateroll', 'fatetext', {
-        name: 'Fate Text',
+        name: 'Title',
         hint: 'This is the text that is displayed above the chosen players name',
         scope: 'world',
         config: true,
@@ -24,6 +24,54 @@ Hooks.once('init', function() {
         default: ' you have been chosen',
         type: String,
     });
+	game.settings.register('fateroll', 'titlecolor', {
+        name: 'Title-Color',
+        hint: 'You can change the title-color of the chat-message. You can use color-names or hex codes.',
+        scope: 'world',
+        config: true,
+        default: 'gold',
+        type: String,
+    });
+	game.settings.register('fateroll', 'textcolor', {
+        name: 'Text-Color',
+        hint: 'You can change the text-color of the chat-message. You can use color-names or hex codes.',
+        scope: 'world',
+        config: true,
+        default: 'gold',
+        type: String,
+    });
+	game.settings.register('fateroll', 'textbackgroundcolor', {
+        name: 'Text-Color',
+        hint: 'You can change the color of the text-background. You can use color-names or hex codes.',
+        scope: 'world',
+        config: true,
+        default: '#00000091',
+        type: String,
+    });
+    game.settings.register('fateroll', 'backgroundcolor', {
+        name: 'Background-Color',
+        hint: 'You can change the background-color of the chat-message. You can use color-names or hex codes.',
+        scope: 'world',
+        config: true,
+        default: 'black',
+        type: String,
+    });
+    game.settings.register('fateroll', 'backgroundimg', {
+        name: 'Background-Image',
+        hint: 'You can change the background-imgae of the chat-message. Put in an url. THIS WILL OVERWRITE THE BACKGROUND COLOR',
+        scope: 'world',
+        config: true,
+        default: '',
+        type: String,
+    });
+	game.settings.register('fateroll', 'soundpath', {
+        name: 'Custom Sound',
+        hint: 'You can change the background-imgae of the chat-message. Put in an url or a path that ends with .wav / .mp3',
+        scope: 'world',
+        config: true,
+        default: 'sounds/dice.wav',
+        type: String,
+    }); 
     console.log("Initialised Fate-Module");
 });
 
@@ -47,39 +95,55 @@ class Fatecontrol {
 	event.preventDefault();
 	let tokens = canvas.tokens.controlled;
 	let randomtoken = tokens[Math.floor(Math.random() * tokens.length)];
-    console.log("TEST");
 	let sHeader = game.settings.get("fateroll", "fatetext");
 	let sPrefix = game.settings.get("fateroll", "prefix");
 	let sSuffix = game.settings.get("fateroll", "suffix");
+	let sBackgroundColor = game.settings.get("fateroll", "backgroundcolor");
+	let sTitleColor = game.settings.get("fateroll", "titlecolor");
+	let sTextColor = game.settings.get("fateroll", "textcolor");
+	let sTextBackgroundColor = game.settings.get("fateroll", "textbackgroundcolor");
+	let sBackgroundImg = game.settings.get("fateroll", "backgroundimg");
+	let sSoundPath = game.settings.get("fateroll", "soundpath");
 	
-    console.log(tokens);
-		let content = `
-		<div style="background: black;" class="dnd5e chat-card item-card">
-			<header class="card-header flexrow">
-				<h3 style="text-align: center;color: gold !important;font-family: inherit;"><b>${sHeader}</b></h3>
-			</header>
-			</br>
-			<h3 style="color:gold; margin-top: -16px;padding: 10px;text-align: center;">${sPrefix}${randomtoken.actor.name}${sSuffix}</h3>
-		</div>`;
-	console.log(randomtoken);
-	console.log(randomtoken.actor.name);
-		let rollWhisper = null;
-            let rollBlind = false;
-            let rollMode = game.settings.get("core", "rollMode");
-            if (["gmroll", "blindroll"].includes(rollMode)) rollWhisper = ChatMessage.getWhisperIDs("GM");
-            if (rollMode === "blindroll") rollBlind = true;
-            ChatMessage.create({
-            user: game.user._id,
-            content: content,
-            blind: rollBlind,
-            sound: CONFIG.sounds.dice,
-            flags: {
-				darksheet: {
-				outcome: 'table'
+	
+	if(tokens.length === 1){
+		ui.notifications.notify("ROLL OF FATE: You only selected one token...");
+	}
+	
+	if(tokens.length === 0){
+		ui.notifications.warn("ROLL OF FATE: You need to select some tokens first...");
+		console.log("Roll of Fate | No tokens selected");
+	}
+	else{
+		console.log(tokens);
+			let content = `
+			<div style="background: ${sBackgroundColor}; background-image: url(${sBackgroundImg});" class="dnd5e chat-card item-card">
+				<header class="card-header flexrow">
+					<h3 style="text-align: center;color: ${sTitleColor} !important;font-family: inherit; background:${sTextBackgroundColor};"><b>${sHeader}</b></h3>
+				</header>
+				</br>
+				<h3 style="color:${sTextColor}; margin-top: -16px;padding: 10px;text-align: center;background:${sTextBackgroundColor};">${sPrefix}${randomtoken.data.name}${sSuffix}</h3>
+			</div>`;
+		console.log(randomtoken);
+		console.log(randomtoken.actor.name);
+			let rollWhisper = null;
+				let rollBlind = false;
+				let rollMode = game.settings.get("core", "rollMode");
+				if (["gmroll", "blindroll"].includes(rollMode)) rollWhisper = ChatMessage.getWhisperIDs("GM");
+				if (rollMode === "blindroll") rollBlind = true;
+				ChatMessage.create({
+				user: game.user._id,
+				content: content,
+				blind: rollBlind,
+				sound: sSoundPath,
+				flags: {
+					darksheet: {
+					outcome: 'table'
+					}
 				}
-            }
-			});
+				});
     }
+	}
 }
 
 Hooks.on('canvasReady', Fatecontrol.addChatControl);
